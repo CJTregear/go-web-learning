1386f3177ef33a9bacdfe4c3fd8f52d875f2bcb6
ctre <ctre85@icloud.com>
2024-02-23 01:49:02 +0000

    add addValues function

--- go-web/main.go
+++ go-web/main.go
@@ -10,7 +10,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the about page")
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	return x + y
 }
 
 func main() {
