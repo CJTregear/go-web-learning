68eba0c407194374f6a6b3f15c79fddee137060c
ctre <ctre85@icloud.com>
2024-02-23 01:40:51 +0000

    Create separate handler with default serve mux

--- go-web/main.go
+++ go-web/main.go
@@ -5,15 +5,17 @@ import (
 	"net/http"
 )
 
-func main() {
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page")
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	_ = http.ListenAndServe(":8080", nil)
 }
