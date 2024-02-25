26872f39993ad2907200d6162fc8b18a671739c2
ctre <ctre85@icloud.com>
2024-02-25 00:33:37 +0000

    comment out basic template caching - render method

--- go-web/pkg/render/render.go
+++ go-web/pkg/render/render.go
@@ -17,29 +17,29 @@ func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 	}
 }
 
-var tc = make(map[string]*template.Template)
-
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
-
-	//check to see if we already have the template in our cache
-	_, inMap := tc[t]
-	if !inMap {
-		err = createTemplateCache(t)
-		log.Println("Creating template and adding to cache")
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		log.Println("Using cached template")
-	}
-	tmpl = tc[t]
-
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Println(err)
-	}
+//var tc = make(map[string]*template.Template)
+//
+//func RenderTemplate(w http.ResponseWriter, t string) {
+//	var tmpl *template.Template
+//	var err error
+//
+//	//check to see if we already have the template in our cache
+//	_, inMap := tc[t]
+//	if !inMap {
+//		err = createTemplateCache(t)
+//		log.Println("Creating template and adding to cache")
+//		if err != nil {
+//			log.Println(err)
+//		}
+//	} else {
+//		log.Println("Using cached template")
+//	}
+//	tmpl = tc[t]
+//
+//	err = tmpl.Execute(w, nil)
+//	if err != nil {
+//		log.Println(err)
+//	}
 }
 
 func createTemplateCache(t string) error {
